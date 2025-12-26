b24d54c26becff15a3dfd6dd77a35750fc47f072
Vito <vitosgh@gmail.com>
2025-12-26 17:57:25 +0700

    fix: update edges_per_token structure in contribution_graph function

--- llm_transparency_tool/components/__init__.py
+++ llm_transparency_tool/components/__init__.py
@@ -61,7 +61,10 @@ def contribution_graph(
         component="graph",
         model_info=model_info.__dict__,
         tokens=tokens,
-        edges_per_token=[nx.node_link_data(g)["links"] for g in graphs],
+        edges_per_token=[
+            [{"source": u, "target": v, **data} for u, v, data in g.edges(data=True)]
+            for g in graphs
+        ],
         default=None,
         key=key,
     )
